b9999baa001b4ac9926cf236e401326246ba2c0e
Maksim Kostromin <daggerok@gmail.com>
2020-02-02 03:31:18 +0200

    the magic is real (C) whatthecommit.com

---
+++ sink-input-app/src/test/java/com/example/sinkinputapp/SinkInputAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.sinkinputapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinkInputAppApplicationTests {
+
+    @Test
+    void contextLoads() { }
+}
